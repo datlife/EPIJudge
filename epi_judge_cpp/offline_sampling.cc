d92bb2d0050c73f0f99b4c4439c1311fd37653c5
datlife <tdat.nguyen93@gmail.com>
2019-05-12 10:42:22 -0700

    Completed 5.12

--- epi_judge_cpp/offline_sampling.cc
+++ epi_judge_cpp/offline_sampling.cc
@@ -2,14 +2,27 @@
 #include <functional>
 #include <iterator>
 #include <vector>
+#include <stdlib.h>
+#include <random>
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
 using std::bind;
 using std::vector;
+using std::random_device;
+using std::uniform_int_distribution;
+using std::default_random_engine;
+
 void RandomSampling(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+	vector<int>& A = *A_ptr;
+	// default_random_engine seed((random_device())());
+	std::size_t size = A.size();
+	for (int i = 0; i < k; i++){
+		// RAND() is not recommended
+		 int idx = std::rand() % (size - i) + i;
+		//int idx = uniform_int_distribution<int>{i, static_cast<int>(size - 1)}(seed);
+		std::swap(A[idx], A[i]);
+	}
 }
 bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
   vector<vector<int>> results;
--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -1,5 +1,5 @@
 import functools
-
+import random
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
     binomial_coefficient, check_sequence_is_uniformly_random,
@@ -8,8 +8,10 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def random_sampling(k, A):
-    # TODO - you fill in here.
-    return
+	for i in range(k):
+		idx = random.randint(i, len(A) - 1)
+		A[i], A[idx] = A[idx], A[i]
+    
 
 
 @enable_executor_hook
