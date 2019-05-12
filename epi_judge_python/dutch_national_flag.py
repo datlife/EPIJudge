63c2b10a63d4369ec1cf15143d98045c5cb634c4
datlife <hi@dat.works>
2019-05-12 01:11:19 -0700

    Added python 5.1"

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -7,9 +7,31 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index, A):
+def dutch_flag_partition(pivot, A):
     # TODO - you fill in here.
-    return
+	if not A:
+		return
+
+	# Runtime: O(N)
+	# Space: O(N)
+	# L, P, R = [], [], []
+	# for i in range(len(A)):
+	#	if A[i] < A[pivot]: L.append(A[i])
+	#	elif A[i] > A[pivot]: R.append(A[i])
+	#	else: P.append(A[i])	
+	# A[:] = L + P + R
+	pivot = A[pivot]
+	smaller, unclassified, larger = 0, 0, len(A)
+	while unclassified < larger:
+		if A[unclassified] < pivot:
+			A[smaller], A[unclassified] = A[unclassified], A[smaller]
+			smaller += 1
+			unclassified += 1
+		elif A[unclassified] == pivot:
+			unclassified += 1
+		else: # A[unclassified] > pivot:
+			larger -= 1
+			A[larger], A[unclassified] = A[unclassified], A[larger]
 
 
 @enable_executor_hook
